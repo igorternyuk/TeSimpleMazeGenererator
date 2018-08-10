01a4754d85a32a4afeab93bb3b068e9164d727ea
igorternyuk <mr.ternyuk@mail.ru>
2018-08-10 10:49:21 +0300

    bug fixing and cleanup

--- mazegenerator.cpp
+++ mazegenerator.cpp
@@ -11,8 +11,9 @@ MazeGenerator::MazeGenerator()
   auto dx = (screenWidth - mWindow.getSize().x) / 2;
   auto dy = (screenHeight - mWindow.getSize().y) / 2;
   mWindow.setPosition(sf::Vector2i(dx, dy));
-  mStack.push(0);
-  mMaze[0] = CELL_VISITED;
+  mStack.push(0x00);
+  mCurrIndex = 0;
+  mMaze[mCurrIndex] |= CELL_VISITED;
   mVisitedCount = 1;
 }
 
@@ -32,7 +33,7 @@ void MazeGenerator::inputPhase() {
     }
     if (event.type == sf::Event::KeyReleased) {
       switch (event.key.code) {
-        case sf::Keyboard::N:
+        case sf::Keyboard::Return:
           newMaze();
           break;
         case sf::Keyboard::Escape:
@@ -51,16 +52,12 @@ void MazeGenerator::inputPhase() {
 
 void MazeGenerator::updatePhase() {
   if (mVisitedCount >= CELLS_IN_WIDTH * CELLS_IN_HEIGHT) {
-      std::cout << "FINISHED" << std::endl;
+      mFinished = true;
       return;
   }
 
-  int currIndex = mStack.top();
-  std::cout << "currIndex =  " << currIndex << std::endl;
-  std::cout << "row =  " << (currIndex / CELLS_IN_WIDTH) << std::endl;
-  std::cout << "col =  " << (currIndex % CELLS_IN_WIDTH) << std::endl;
-  //mStack.pop();
-  auto neighbours = getNeighbours(currIndex);
+  mCurrIndex = mStack.top();
+  auto neighbours = getNeighbours(mCurrIndex);
   /*std::vector<int> unvisitedNeighbours;
   std::copy_if(neighbours.begin(), neighbours.end(),
                  std::back_inserter(unvisitedNeighbours), [](int index){
@@ -77,19 +74,19 @@ void MazeGenerator::updatePhase() {
       Direction neighbourDir = neighbours[randNeighbourIndex].second;
       mStack.push(randNeighbourIndexInMaze);
       if(neighbourDir == Direction::NORTH){
-          mMaze[currIndex] |= CELL_PATH_NORTH;
+          mMaze[mCurrIndex] |= CELL_PATH_NORTH;
           mMaze[randNeighbourIndexInMaze] |= CELL_PATH_SOUTH;
       } else if(neighbourDir == Direction::SOUTH){
-          mMaze[currIndex] |= CELL_PATH_SOUTH;
+          mMaze[mCurrIndex] |= CELL_PATH_SOUTH;
           mMaze[randNeighbourIndexInMaze] |= CELL_PATH_NORTH;
       } else if(neighbourDir == Direction::EAST){
-          mMaze[currIndex] |= CELL_PATH_EAST;
+          mMaze[mCurrIndex] |= CELL_PATH_EAST;
           mMaze[randNeighbourIndexInMaze] |= CELL_PATH_WEST;
       } else if(neighbourDir == Direction::WEST){
-          mMaze[currIndex] |= CELL_PATH_WEST;
+          mMaze[mCurrIndex] |= CELL_PATH_WEST;
           mMaze[randNeighbourIndexInMaze] |= CELL_PATH_EAST;
       }
-      mMaze[currIndex] |= CELL_VISITED;
+      mMaze[randNeighbourIndexInMaze] |= CELL_VISITED;
       ++mVisitedCount;
   } else {
       mStack.pop();
@@ -99,32 +96,35 @@ void MazeGenerator::updatePhase() {
 
 void MazeGenerator::renderingPhase() {
   mWindow.clear(sf::Color::Black);
-  int counter = 0;
   for (size_t i{0u}; i < mMaze.size(); ++i) {
     auto spot = getCoordsByIndex(i);
     auto position = sf::Vector2i(spot.x * CELL_SIZE, spot.y * CELL_SIZE);
     sf::RectangleShape cell;
     cell.setPosition(position.x, position.y);
     cell.setSize(sf::Vector2f(CELL_SIZE, CELL_SIZE));
-    cell.setFillColor(sf::Color::Red);
+    cell.setFillColor(mWallColor);
     mWindow.draw(cell);
 
     cell.move(WALL_WIDTH, WALL_WIDTH);
     cell.setSize(sf::Vector2f(CELL_SIZE - WALL_WIDTH, CELL_SIZE - WALL_WIDTH));
-    if (mMaze[i] & CELL_VISITED) {
-      cell.setFillColor(sf::Color::Cyan);
+    if(!mFinished && i == mCurrIndex){
+        cell.setFillColor(sf::Color::Green);
     } else {
-      cell.setFillColor(sf::Color::Blue);
+        if (mMaze[i] & CELL_VISITED) {
+          cell.setFillColor(mVisitedColor);
+        } else {
+          cell.setFillColor(mUnvisitedColor);
+        }
     }
+
     mWindow.draw(cell);
-    cell.setSize(sf::Vector2f(-CELL_SIZE, -CELL_SIZE));
 
     if (mMaze[i] & CELL_PATH_NORTH) {
       sf::RectangleShape northernPath;
       northernPath.setPosition(position.x + WALL_WIDTH, position.y);
       northernPath.setSize(
-          sf::Vector2f(CELL_SIZE - 2 * WALL_WIDTH, WALL_WIDTH));
-      northernPath.setFillColor(sf::Color::Cyan);
+          sf::Vector2f(CELL_SIZE - WALL_WIDTH, WALL_WIDTH));
+      northernPath.setFillColor(sf::Color(127,0,110));
       mWindow.draw(northernPath);
     }
 
@@ -133,8 +133,8 @@ void MazeGenerator::renderingPhase() {
       southernPath.setPosition(position.x + WALL_WIDTH,
                                position.y + CELL_SIZE - WALL_WIDTH);
       southernPath.setSize(
-          sf::Vector2f(CELL_SIZE - 2 * WALL_WIDTH, WALL_WIDTH));
-      southernPath.setFillColor(sf::Color::Cyan);
+          sf::Vector2f(CELL_SIZE - WALL_WIDTH, WALL_WIDTH));
+      southernPath.setFillColor(mVisitedColor);
       mWindow.draw(southernPath);
     }
 
@@ -142,24 +142,33 @@ void MazeGenerator::renderingPhase() {
       sf::RectangleShape easternPath;
       easternPath.setPosition(position.x + CELL_SIZE - WALL_WIDTH,
                               position.y + WALL_WIDTH);
-      easternPath.setSize(sf::Vector2f(WALL_WIDTH, CELL_SIZE - 2 * WALL_WIDTH));
-      easternPath.setFillColor(sf::Color::Cyan);
+      easternPath.setSize(sf::Vector2f(WALL_WIDTH, CELL_SIZE - WALL_WIDTH));
+      easternPath.setFillColor(mVisitedColor);
       mWindow.draw(easternPath);
     }
 
     if (mMaze[i] & CELL_PATH_WEST) {
       sf::RectangleShape westernPath;
       westernPath.setPosition(position.x, position.y + WALL_WIDTH);
-      westernPath.setSize(sf::Vector2f(WALL_WIDTH, CELL_SIZE - 2 * WALL_WIDTH));
-      westernPath.setFillColor(sf::Color::Cyan);
+      westernPath.setSize(sf::Vector2f(WALL_WIDTH, CELL_SIZE -  WALL_WIDTH));
+      westernPath.setFillColor(mVisitedColor);
       mWindow.draw(westernPath);
     }
-    ++counter;
   }
   mWindow.display();
 }
 
-void MazeGenerator::newMaze() {}
+void MazeGenerator::newMaze() {
+  std::fill(mMaze.begin(), mMaze.end(), 0x00);
+  while (!mStack.empty()) {
+      mStack.pop();
+  }
+  mStack.push(0x00);
+  mCurrIndex = 0;
+  mMaze[mCurrIndex] |= CELL_VISITED;
+  mVisitedCount = 1;
+  mFinished = false;
+}
 
 int MazeGenerator::getIndexByCoords(int x, int y) {
   return y * CELLS_IN_WIDTH + x;
@@ -171,21 +180,22 @@ sf::Vector2i MazeGenerator::getCoordsByIndex(int index) {
   return sf::Vector2i(x, y);
 }
 
-bool MazeGenerator::isIndexValid(int index) {
-  return index >= 0 && index < mMaze.size();
+bool MazeGenerator::areCoordinatesValid(int x, int y)
+{
+  return x >= 0 && x < CELLS_IN_WIDTH && y >= 0 && y < CELLS_IN_HEIGHT;
 }
 
 std::vector<std::pair<int, MazeGenerator::Direction> > MazeGenerator::getNeighbours(int index) {
   std::vector<std::pair<int, Direction>> neighbours;
-  std::vector<sf::Vector2i> offsets{{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+  std::vector<sf::Vector2i> offsets{{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
   auto currCellPosition = getCoordsByIndex(index);
-  for (int i = 0; i < offsets.size(); ++i) {
+  for (size_t i {0u}; i < offsets.size(); ++i) {
     Direction dir = static_cast<Direction>(i);
     auto offset = offsets[i];
     int nx = currCellPosition.x + offset.x;
     int ny = currCellPosition.y + offset.y;
-    int neighbourIndex = getIndexByCoords(nx, ny);
-    if (isIndexValid(neighbourIndex)) {
+    if (areCoordinatesValid(nx, ny)) {
+      int neighbourIndex = getIndexByCoords(nx, ny);
       neighbours.push_back(std::make_pair(neighbourIndex, dir));
     }
   }
--- mazegenerator.hpp
+++ mazegenerator.hpp
@@ -14,34 +14,39 @@ class MazeGenerator {
 
  private:
   enum {
-    CELL_SIZE = 128,
-    WALL_WIDTH = 4,
-    CELLS_IN_WIDTH = 3,
-    CELLS_IN_HEIGHT = 3,
+    CELL_SIZE = 32,
+    WALL_WIDTH = 6,
+    CELLS_IN_WIDTH = 20,
+    CELLS_IN_HEIGHT = 20,
     WINDOW_WIDTH = CELLS_IN_WIDTH * CELL_SIZE,
     WINDOW_HEIGHT = CELLS_IN_HEIGHT * CELL_SIZE
   };
-  enum class Direction { NORTH, SOUTH, EAST, WEST };
+  enum class Direction { NORTH, EAST, SOUTH, WEST };
   enum {
     CELL_PATH_NORTH = 0x01,
-    CELL_PATH_SOUTH = 0x02,
-    CELL_PATH_EAST = 0x04,
+    CELL_PATH_EAST = 0x02,
+    CELL_PATH_SOUTH = 0x04,
     CELL_PATH_WEST = 0x08,
     CELL_VISITED = 0x10
   };
   std::vector<int> mMaze;
   std::stack<int> mStack;
-  int mVisitedCount{0};
+  size_t mCurrIndex;
+  size_t mVisitedCount{0};
+  bool mFinished {false};
   Random random;
   const std::string WINDOW_TITLE{"MazeGenerator"};
   sf::RenderWindow mWindow;
+  sf::Color mWallColor {sf::Color::White};
+  sf::Color mVisitedColor {sf::Color(127,0,110)};
+  sf::Color mUnvisitedColor {sf::Color(64,64,64)};
   void inputPhase();
   void updatePhase();
   void renderingPhase();
   void newMaze();
   int getIndexByCoords(int x, int y);
   sf::Vector2i getCoordsByIndex(int index);
-  bool isIndexValid(int index);
+  bool areCoordinatesValid(int x, int y);
   std::vector<std::pair<int, Direction> > getNeighbours(int index);
 };
 
